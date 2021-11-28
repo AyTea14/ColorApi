9a3451b2c1fe8c1a9af566037a8fb1f8d17d3c83
AyTea14 <85985879+AyTea14@users.noreply.github.com>
2021-11-28 16:34:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -17,23 +17,33 @@ app.get("/", (request, response) => {
         .end("Welcome to ColorAPI! For documentation and info go to https://colorapi.advaith.fun kthx");
 });
 
-app.get("/*.svg", (request, response) => {
-    const hex = request.originalUrl.replace("/", "").replace(".svg", "");
+app.get("/*/*.svg", (request, response) => {
+    let data = request.originalUrl.split("/");
+    const [, ...mydata] = data;
+    const [x, y] = mydata[0].split("x");
+    const hex = mydata[1].replace(".svg", "");
+
+    // const hex = request.originalUrl.replace("/", "").replace(".svg", "");
 
     response
         .status(200)
         .set("Content-Type", "image/svg+xml")
         .end(
-            `<svg xmlns="http://www.w3.org/2000/svg" width="256" height="256"><rect fill="#${hex}" x="0" y="0" width="256" height="256"></rect></svg>`
+            `<svg xmlns="http://www.w3.org/2000/svg" width="${x}" height="${y}"><rect fill="#${hex}" x="0" y="0" width="${x}" height="${y}"></rect></svg>`
         );
 });
 
 app.get("/*.webp", async (request, response) => {
-    const hex = request.originalUrl.replace("/", "").replace(".webp", "").replace("#", "");
+    let data = request.originalUrl.split("/");
+    const [, ...mydata] = data;
+    const [x, y] = mydata[0].split("x");
+    const hex = mydata[1].replace(".webp", "");
+
+    // const hex = request.originalUrl.replace("/", "").replace(".webp", "").replace("#", "");
 
     const image = await svgToImg
         .from(
-            `<svg xmlns="http://www.w3.org/2000/svg" width="256" height="256"><rect fill="#${hex}" x="0" y="0" width="256" height="256"></rect></svg>`
+            `<svg xmlns="http://www.w3.org/2000/svg" width="${x}" height="${y}"><rect fill="#${hex}" x="0" y="0" width="${x}" height="${y}"></rect></svg>`
         )
         .toWebp()
         .catch((error) => response.status(500).set("Content-Type", "text/plain").end(String(error)));
@@ -41,12 +51,15 @@ app.get("/*.webp", async (request, response) => {
     response.status(200).set("Content-Type", "image/webp").end(image);
 });
 
-app.get("/*.png", async (request, response) => {
-    const hex = request.originalUrl.replace("/", "").replace(".png", "").replace("#", "");
+app.get("/*/*.png", async (request, response) => {
+    let data = request.originalUrl.split("/");
+    const [, ...mydata] = data;
+    const [x, y] = mydata[0].split("x");
+    const hex = mydata[1].replace(".png", "");
 
     const image = await svgToImg
         .from(
-            `<svg xmlns="http://www.w3.org/2000/svg" width="256" height="256"><rect fill="#${hex}" x="0" y="0" width="256" height="256"></rect></svg>`
+            `<svg xmlns="http://www.w3.org/2000/svg" width="${x}" height="${y}"><rect fill="#${hex}" x="0" y="0" width="256" height="256"></rect></svg>`
         )
         .toPng()
         .catch((error) => response.status(500).set("Content-Type", "text/plain").end(String(error)));
@@ -54,12 +67,15 @@ app.get("/*.png", async (request, response) => {
     response.status(200).set("Content-Type", "image/png").end(image);
 });
 
-app.get("/*.jpeg", async (request, response) => {
-    const hex = request.originalUrl.replace("/", "").replace(".jpeg", "").replace("#", "");
+app.get("/*/*.jpeg", async (request, response) => {
+    let data = request.originalUrl.split("/");
+    const [, ...mydata] = data;
+    const [x, y] = mydata[0].split("x");
+    const hex = mydata[1].replace(".jpeg", "");
 
     const image = await svgToImg
         .from(
-            `<svg xmlns="http://www.w3.org/2000/svg" width="256" height="256"><rect fill="#${hex}" x="0" y="0" width="256" height="256"></rect></svg>`
+            `<svg xmlns="http://www.w3.org/2000/svg" width="${x}" height="${y}"><rect fill="#${hex}" x="0" y="0" width="${x}" height="${y}"></rect></svg>`
         )
         .toJpeg()
         .catch((error) => response.status(500).set("Content-Type", "text/plain").end(String(error)));
@@ -67,12 +83,15 @@ app.get("/*.jpeg", async (request, response) => {
     response.status(200).set("Content-Type", "image/jpeg").end(image);
 });
 
-app.get("/*.jpg", async (request, response) => {
-    const hex = request.originalUrl.replace("/", "").replace(".jpg", "").replace("#", "");
+app.get("/*/*.jpg", async (request, response) => {
+    let data = request.originalUrl.split("/");
+    const [, ...mydata] = data;
+    const [x, y] = mydata[0].split("x");
+    const hex = mydata[1].replace(".jpg", "");
 
     const image = await svgToImg
         .from(
-            `<svg xmlns="http://www.w3.org/2000/svg" width="256" height="256"><rect fill="#${hex}" x="0" y="0" width="256" height="256"></rect></svg>`
+            `<svg xmlns="http://www.w3.org/2000/svg" width="${x}" height="${y}"><rect fill="#${hex}" x="0" y="0" width="${x}" height="${y}"></rect></svg>`
         )
         .toJpeg()
         .catch((error) => response.status(500).set("Content-Type", "text/plain").end(String(error)));
@@ -90,8 +109,8 @@ app.use(function (request, response) {
 
 app.listen(process.env["PORT"] || 3000);
 
-setInterval(() => {
-    http.get(`http://ColorApi.aytea14.repl.co/`);
-}, 280000);
+// setInterval(() => {
+//     http.get(`http://ColorApi.aytea14.repl.co/`);
+// }, 280000);
 
 console.log("Started!");
